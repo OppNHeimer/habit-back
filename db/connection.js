20b8c6bff39ff20056d71aa89e674d3f45dc6835
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-03-15 19:18:57 -0400

    mod connection.js for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 // mongoose.connect('mongodb://127.0.0.1/habits')
 
-mongoose.connect('mongodb://localhost/habits')
+if (process.env.NODE_ENV == 'production') {
+    mongoose.connect(process.env.MLAB_URL)
+} else {
+    mongoose.connect('mongodb://localhost/habits')
+}
 
 mongoose.Promise = Promise
 
