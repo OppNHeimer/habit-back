0ebc16febdc05e45e6b3cec4f16968b3c68a17e8
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-03-16 17:35:07 -0400

    testing seeds

--- db/seed.js
+++ db/seed.js
@@ -7,22 +7,36 @@ const userSeeds = require('./userSeeds.json')
 console.log('hello from seed.js')
 Habit.remove({})
   .then(() => {
-    Habit.collection.insert(habitSeeds)
-    .then(habits => {
-      console.log('Seeded habits:')
-      console.log(JSON.stringify(habits, null, 2))
-      // process.exit()
+    Habit.create({
+      email: "email",
+      password: "password",
+      activeHabits: 0
+    })
+    .then(res => {
+      console.log(res)
     })
   })
-  .catch(error => console.log(error))
+  .then(() => { process.exit()})
+  .catch(err => {console.log(err)}
+)
 
-User.remove({})
-  .then(() => {
-    User.collection.insert(userSeeds)
-    .then(user => {
-      console.log('Seeded users:')
-      console.log(JSON.stringify(users, null, 2))
-      process.exit()
-    })
-  })
-  .catch(error => console.log(error))
+
+//     Habit.collection.insert(habitSeeds)
+//     .then(habits => {
+//       console.log('Seeded habits:')
+//       console.log(JSON.stringify(habits, null, 2))
+//       // process.exit()
+//     })
+//   })
+//   .catch(error => console.log(error))
+
+// User.remove({})
+//   .then(() => {
+//     User.collection.insert(userSeeds)
+//     .then(user => {
+//       console.log('Seeded users:')
+//       console.log(JSON.stringify(users, null, 2))
+//       process.exit()
+//     })
+//   })
+//   .catch(error => console.log(error))
