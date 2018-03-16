91ce0e14136a9c0015a15b4142a417ea26b0678e
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-03-16 14:27:05 -0400

    cors

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const usersController = require('./usersController')
 const app = express()
 
 app.use(parser.json())
-app.use(cors())
-app.options('*', cors())
+// app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+// app.options('*', cors())
 
 app.use('/auth', cors(), usersController)
 app.use('/', passport.authenticate('jwt', { session: false }), habitsController)
