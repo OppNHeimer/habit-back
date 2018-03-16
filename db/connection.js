44e712d1e21b4329d89037a50465caa9e55419bc
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-03-16 15:38:35 -0400

    testing mlab url

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 // mongoose.connect('mongodb://127.0.0.1/habits')
 
-if (process.env.NODE_ENV == 'production') {
-    mongoose.connect(process.env.MLAB_URL)
-} else {
-    mongoose.connect('mongodb://localhost/habits')
-}
-
+// if (process.env.NODE_ENV == 'production') {
+    // mongoose.connect(process.env.MLAB_URL)
+// } else {
+//     mongoose.connect('mongodb://localhost/habits')
+// }
+mongoose.connect('mongodb://habit100:bratSwag44%hab@ds115569.mlab.com:15569/habit100')
 mongoose.Promise = Promise
 
 module.exports = mongoose
