d79e07931be7b481cbad15002a57baee3cc57da9
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-03-16 13:58:41 -0400

    config cors

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const usersController = require('./usersController')
 const app = express()
 
 app.use(parser.json())
-app.use(cors())
+// app.use(cors())
+app.options('*', cors())
 
 app.use('/auth', usersController)
 app.use('/', passport.authenticate('jwt', { session: false }), habitsController)
